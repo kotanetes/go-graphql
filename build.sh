510ed6bc6877468977507f2736b7f1dccabf9b05
Sai Kota <33044622+kotanetes@users.noreply.github.com>
2020-01-04 03:15:56 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ docker container create --name cont-extract go-graphql:build
 docker container cp cont-extract:/go-graphql/app ./app
 #build slim image
 docker build -t go-graphql:latest -f dockerfile.release .
+#remove docker container
+docker rm cont-extract
+#remove build image
+docker rmi -f go-graphql:build
+rm ./app
